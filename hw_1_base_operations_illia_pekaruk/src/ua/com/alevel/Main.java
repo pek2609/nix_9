d8d53e23451e52b4f4b1b98c1e0d4e8df9015952
pek2609 <pekaruk.ilyaa@gmail.com>
2021-10-10 01:04:13 +0300

    creating new module

---
+++ hw_1_base_operations_illia_pekaruk/src/ua/com/alevel/Main.java
@@ -0,0 +1,14 @@
+package ua.com.alevel;
+
+import ua.com.alevel.tasks.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        Task2 task2 = new Task2();
+        Task3 task3 = new Task3();
+
+        task1.run();
+        task3.run();
+    }
+}
---
+++ hw_1_base_operations_illia_pekaruk/src/ua/com/alevel/tasks/Task2.java
@@ -0,0 +1,9 @@
+package ua.com.alevel.tasks;
+
+
+public class Task2 {
+    public void run() {
+    }
+
+
+}
