5f91dd318136a306847bfab8136d039dc76f410d
pek2609 <pekaruk.ilyaa@gmail.com>
2021-10-11 18:53:32 +0300

    Task2 implementation

--- hw_1_base_operations/src/main/java/ua/com/alevel/tasks/Task2.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/tasks/Task2.java
@@ -1,7 +1,32 @@
 package ua.com.alevel.tasks;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Task2 {
     public void run() {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the string:");
+        String enterString ="";
+        char[] symbols = new char[0];
+        try {
+            enterString = bufferedReader.readLine();
+            symbols = enterString.toCharArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Map<Character, Integer> characterIntegerMap = new TreeMap<>();
+        for (char symbol : symbols) {
+            if(Character.isLetter(symbol)){
+                int lenDiff = enterString.length() - enterString.replace(Character.toString(symbol), "").length();
+                characterIntegerMap.put(symbol, lenDiff);
+            }
+        }
+        System.out.println(characterIntegerMap.toString());
     }
 }
