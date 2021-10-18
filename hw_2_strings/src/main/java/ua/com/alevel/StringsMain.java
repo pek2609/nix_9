edc8183b7a0a2dca3c7fc9fedfe26b7310b4603e
pek2609 <pekaruk.ilyaa@gmail.com>
2021-10-19 00:22:55 +0300

    create class StringsMain

---
+++ hw_2_strings/src/main/java/ua/com/alevel/StringsMain.java
@@ -0,0 +1,9 @@
+package ua.com.alevel;
+
+import java.util.Arrays;
+
+public class StringsMain {
+    public static void main(String[] args) {
+        System.out.println(StringReverseUtils.reverse("hello world", 3, 10));
+    }
+}
