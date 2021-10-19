00e84906eb4756fc5f72d42932cd86aaa1b9a44f
pek2609 <pekaruk.ilyaa@gmail.com>
2021-10-19 13:57:58 +0300

    add checking exceptional situations in StringReverseUtils

--- string_helper/src/main/java/ua/com/alevel/StringReverseUtils.java
+++ string_helper/src/main/java/ua/com/alevel/StringReverseUtils.java
@@ -1,46 +1,65 @@
 package ua.com.alevel;
 
 
+import org.apache.commons.lang3.StringUtils;
+
 public final class StringReverseUtils {
 
     private StringReverseUtils() {
     }
 
+    private static final String EMPTY_STRING_MESSAGE = "String is Empty";
 
     public static String reverse(String src, String dest) {
-        if (src.contains(dest)) {
-            return src.replaceAll(dest, reverse(dest));
-        } else {
-            return "No matching";
+        if (StringUtils.isNoneBlank(src)&&StringUtils.isNoneBlank(dest)) {
+            if (src.contains(dest)) {
+                return src.replaceAll(dest, reverse(dest));
+            } else {
+                return "No matching";
+            }
         }
+        else return EMPTY_STRING_MESSAGE;
     }
 
     public static String reverse(String src, int firstIndex, int lastIndex) {
-        String subString = src.substring(firstIndex, lastIndex + 1);
-        String subStringReversed;
-        if (subString.contains(" ")) {
-            subStringReversed = reverseByWord(subString);
-        } else {
-            subStringReversed = reverse(subString);
-        }
-        return src.replaceAll(subString, subStringReversed);
+        if (StringUtils.isNoneBlank(src)) {
+            if(!isOutOfBounds(src, firstIndex, lastIndex)) {
+                String subString = src.substring(firstIndex, lastIndex + 1);
+                String subStringReversed;
+                if (subString.contains(" ")) {
+                    subStringReversed = reverseByWord(subString);
+                } else {
+                    subStringReversed = reverse(subString);
+                }
+                return src.replaceAll(subString, subStringReversed);
+            }
+            else return "Indexes are unsuitable!";
+        } else return EMPTY_STRING_MESSAGE;
     }
 
     public static String reverse(String src) {
-        char[] srcChars = src.toCharArray();
-        char[] reverseChars = new char[src.length()];
-        for (int i = srcChars.length - 1, j = 0; i >= 0; i--) {
-            reverseChars[j++] = srcChars[i];
-        }
-        return String.copyValueOf(reverseChars);
+        if (StringUtils.isNoneBlank(src)) {
+            char[] srcChars = src.toCharArray();
+            char[] reverseChars = new char[src.length()];
+            for (int i = srcChars.length - 1, j = 0; i >= 0; i--) {
+                reverseChars[j++] = srcChars[i];
+            }
+            return String.copyValueOf(reverseChars);
+        } else return EMPTY_STRING_MESSAGE;
+    }
+
+    private static boolean isOutOfBounds(String src, int firstIndex, int lastIndex) {
+        return firstIndex*lastIndex<0||lastIndex>src.length()||firstIndex>src.length()||firstIndex>lastIndex;
     }
 
     public static String reverseByWord(String src) {
-        String[] words = src.split(" ");
-        for (int i = 0; i < words.length; i++) {
-            words[i] = reverse(words[i]);
-        }
-        return String.join(" ", words);
+        if (StringUtils.isNoneBlank(src)) {
+            String[] words = src.split(" ");
+            for (int i = 0; i < words.length; i++) {
+                words[i] = reverse(words[i]);
+            }
+            return String.join(" ", words);
+        } else return EMPTY_STRING_MESSAGE;
     }
 
 }
