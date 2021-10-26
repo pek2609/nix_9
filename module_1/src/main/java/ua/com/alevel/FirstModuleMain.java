43f0ffd5fc5e5de7806681cc436377fcbcf46d4a
pek2609 <pekaruk.ilyaa@gmail.com>
2021-10-26 16:51:58 +0300

    add console runner to FirstModuleMain

--- module_1/src/main/java/ua/com/alevel/FirstModuleMain.java
+++ module_1/src/main/java/ua/com/alevel/FirstModuleMain.java
@@ -1,9 +1,16 @@
 package ua.com.alevel;
 
-import ua.com.alevel.firstlevel.NumberOfUniqueSymbols;
+import ua.com.alevel.firstlevel.uniquenumbers.NumberOfUniqueNumbers;
+import ua.com.alevel.firstlevel.knightmove.KnightMove;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstModuleMain {
     public static void main(String[] args) {
-            new NumberOfUniqueSymbols().run();
+        List<TaskRunner> taskRunnerList = new ArrayList<>();
+        taskRunnerList.add(new NumberOfUniqueNumbers());
+        taskRunnerList.add(new KnightMove());
+        ConsoleHelper.run(taskRunnerList);
     }
 }
